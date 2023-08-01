decc1b7bb3a3d3643ed0314f46b271fa34e894a6
Vishnu <nvishnu1995@gmail.com>
2023-08-01 11:18:34 +0530

    changed backend url

--- src/Components/data/constants.js
+++ src/Components/data/constants.js
@@ -1,3 +1,1 @@
-export const BACKEND_API = "https://amazon-prime-backend-production.up.railway.app"
-
-// https://protected-chamber-55418.herokuapp.com
+export const BACKEND_API = "https://plain-culottes-fawn.cyclic.cloud"
